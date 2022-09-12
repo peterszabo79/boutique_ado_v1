f273567cb59a376da206b35a54ca45d169ac1951
peterszabo79 <peterszabo79@gmail.com>
2022-09-12 20:39:48 +0000

    DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -167,6 +167,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
