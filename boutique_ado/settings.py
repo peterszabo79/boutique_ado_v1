877b2346cf687f1461c777b4c6a621b844e1c67f
peterszabo79 <peterszabo79@gmail.com>
2022-09-20 21:07:55 +0000

    fix heroku problem

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,13 +120,16 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse('postgres://ktuvfrrintqvkp:465f9871f28832b05fc222ce7ade3f4a0410d7c5842994e05d63ddfeaed6b33a@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/d7r1r1aqtr4ltv')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
